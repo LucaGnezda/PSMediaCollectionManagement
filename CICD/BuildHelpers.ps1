7ce5b14927233f9782ba1c3c3d1462e7b930b45b
Luca Gnezda <luca.gnezda@live.com.au>
2022-02-06 15:20:20 +1030

    Refining Build process

--- CICD/BuildHelpers.ps1
+++ CICD/BuildHelpers.ps1
@@ -49,8 +49,8 @@ function New-PesterDetailedTestConfiguration ([Switch] $IgnoreRemoteFilesystem, 
 
     $config = New-PesterConfiguration
     $config.Run.PassThru = $true
-    $config.Run.Path = "$PSScriptRoot\..\Tests\*.Tests.ps1"
-    #$config.Run.Path = "$PSScriptRoot\..\Tests\31.PS.MCM.CM.OM.Actor.Tests.ps1"
+    #$config.Run.Path = "$PSScriptRoot\..\Tests\*.Tests.ps1"
+    $config.Run.Path = "$PSScriptRoot\..\Tests\31.*.Tests.ps1"
     $config.Filter.ExcludeTag = $exclusions.ToArray()
     $config.Should.ErrorAction = "Continue"
     $config.Output.Verbosity = "Detailed"
